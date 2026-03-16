0b0d0d547a96842a8a81d7c4152bd6054a2e7418
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-16 19:13:23 +0100

    implementazione logica e aggiunta dei generi dei film a schermo

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,53 @@ const movies =
  ]
 
 function App(){
+  const [selectedGenre , setSelectedGenre] = useState ("")
+
+  //qui e se l'utente ha selezionato qualcosa oppure la space bar e vuota
+  const filteredMovies = selectedGenre === '' 
+    //se l'utente non ha scelto un genre specifico ci da una copia dell'arrey intero
+    ? movies 
+    //se l'utente sceglie un genere usa movies.filter creando un nuovo arrey con solo quel genre
+    : movies.filter(movie => movie.genre === selectedGenre)
+
+  const allGenres = movies.map(movie => movie.genre)
   
-  const [filteredGenre , setFilteredGenre] = useState ("")
+  const uniqueGenres = allGenres.filter((genre, index) => {
+  return allGenres.indexOf(genre) === index;
+  });
 
   return (
     <>
       <div className="container">
         <h1>CHE TIPO SEI STASERA</h1>
-        <select className="form-select" onChange={(e)=>setFilteredGenre(e.target.value)} value={filteredGenre}>
+
+        {/*qui al click dell'utente overo la nostra 
+          e-evento gli diciamo di prendere il valore 
+          cliccato e salvarlo in setSelectedGenre mentre 
+          value ci fara mostare a schermo il risultato 
+          di selected genre*/}
+        <select className="form-select" onChange={(e)=>setSelectedGenre(e.target.value)} value={selectedGenre}>
           <option value="">GENERI</option>
-          {movies.map((movie,genre)=>(
-            <option key={genre} value={movie.genre}>
-              {movie.genre}
+
+          {/*qui effettuiamo il ciclo map e per ogni 
+            genere unico ci facciamo un option*/}
+          {uniqueGenres.map((genre)=>(
+            <option key={genre} value={genre}>{genre}
             </option>
           ))}
         </select>
+
+        <ul className="list-group mt-3">
+          {/*qui usiamo l'arrey filtrato per far 
+            visualizzare soltanto l'opzione scelta 
+            dall'utente*/}
+        {filteredMovies.map((movie, index) => (
+          <li key={index} className="list-group-item">
+            {movie.title} - <small>{movie.genre}</small>
+          </li>
+        ))}
+      </ul>
+
       </div>
     </>
   )
